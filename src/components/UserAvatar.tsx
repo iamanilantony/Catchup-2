58f931afc01cc3cd0140526644a00b133898b982
Anil Antony <anilantony3134@gmail.com>
2023-07-16 08:50:25 +0530

    Feat: Added login flow

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,34 +1,32 @@
-import { User } from 'next-auth'
-import { FC } from 'react'
-import { Avatar, AvatarFallback } from './ui/avatar'
-import Image from 'next/image'
-import { Icons } from './Icons'
+import { User } from '@prisma/client'
 import { AvatarProps } from '@radix-ui/react-avatar'
 
-interface UserAvatarProps extends AvatarProps{
-  user: Pick<User, 'name' | 'image'>
+import { Icons } from '@/components/Icons'
+import { Avatar, AvatarFallback } from '@/components/ui/Avatar'
+import Image from 'next/image'
+
+interface UserAvatarProps extends AvatarProps {
+  user: Pick<User, 'image' | 'name'>
 }
 
-const UserAvatar: FC<UserAvatarProps> = ({ user, ...props }) => {
+export function UserAvatar({ user, ...props }: UserAvatarProps) {
   return (
     <Avatar {...props}>
-        {user.image ? (
-            <div className='relative aspect-square h-full w-full'>
-                <Image
-                    fill
-                    src={user.image}
-                    alt='profile picture'
-                    referrerPolicy='no-referrer'
-                />
-            </div>
-        ): (
-            <AvatarFallback>
-                <span className='sr-only'>{user?.name}</span>
-                <Icons.user className='h-4 w-4'/>
-            </AvatarFallback>
-        )}
+      {user.image ? (
+        <div className='relative aspect-square h-full w-full'>
+          <Image
+            fill
+            src={user.image}
+            alt='profile picture'
+            referrerPolicy='no-referrer'
+          />
+        </div>
+      ) : (
+        <AvatarFallback>
+          <span className='sr-only'>{user?.name}</span>
+          <Icons.user className='h-4 w-4' />
+        </AvatarFallback>
+      )}
     </Avatar>
   )
 }
-
-export default UserAvatar
---
+++ src/components/ui/CloseModal.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { FC } from "react";
+import { Button } from "./Button";
+
+interface CloseModalProps {}
+
+const CloseModal: FC<CloseModalProps> = ({}) => {
+  const router = useRouter();
+  console.log(router);
+
+  return (
+    <Button
+      variant="subtle"
+      className="h-6 w-6 p-0 rounded-md"
+      onClick={() => router.back()}
+    >
+      <X aria-label="close modal" className="h-4 w-4" />
+    </Button>
+  );
+};
+
+export default CloseModal;
