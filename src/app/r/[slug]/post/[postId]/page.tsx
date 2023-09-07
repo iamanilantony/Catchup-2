31a2af79a1132a906b47d6a36d93c408dfe8b3ee
Anil Antony <anilantony3134@gmail.com>
2023-09-07 10:24:27 +0530

    fix: Added page issue fi

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,3 +1,4 @@
+import EditorOutput from "@/components/EditorOutput";
 import PostVoteServer from "@/components/post-vote/PostVoteServer";
 import { buttonVariants } from "@/components/ui/Button";
 import { db } from "@/lib/db";
@@ -7,6 +8,7 @@ import { Post, User, Vote } from "@prisma/client";
 import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
 import { notFound } from "next/navigation";
 import { FC, Suspense } from "react";
+// import {formatTimeToNow} from "@/lib/utils"
 
 interface pageProps {
   params: {
@@ -57,8 +59,26 @@ const page = async ({ params }: pageProps) => {
             }}
           />
         </Suspense>
-      </div>
-    </div>
+        
+        <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+            Posted by u/{post?.author.username ?? cachedPost.authorUsername}{' '}
+            {/* {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))} */}
+            </p>
+            <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+              {post?.title ?? cachedPost.title}
+            </h1>
+            <EditorOutput content={post?.content ?? cachedPost.content} />
+            <Suspense
+              fallback={
+                <Loader2 className="h-5 w-5 animate-spin text-zinc-500"/>
+              }
+            >
+              {/* <CommentsSection /> */}
+            </Suspense>
+            </div>
+          </div>
+        </div>
   );
 };
 
