d0e645b9825c69f1a5c4b0c5a707794c61d60853
Anil Antony <anilantony3134@gmail.com>
2023-12-03 20:26:38 +0530

    chore: added tailwind

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
-  return <h1>Hello, Dashboard Page!</h1>;
+  return (
+    <>
+      <h1 className="bg-black text-center">Hello, Dashboard Page!</h1>
+      <div className="flex flex-col">
+        <span className="mr-2">Hi</span>
+        <span className="mr-2">Hi</span>
+        <span className="mr-2">Hi</span>
+      </div>
+    </>
+  );
 }
