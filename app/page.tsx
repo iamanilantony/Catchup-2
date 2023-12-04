bb331a0a867a0f055cadf87c5d27f40b9f01acd0
Anil Antony <anilantony3134@gmail.com>
2023-12-05 00:45:46 +0530

    chore: added dialogbox

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,14 @@
 
 import HeadSEO from "../components/HeadSEO";
 import { Button } from "../components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "../components/ui/dialog";
 
 export default function Home() {
   return (
@@ -14,7 +22,20 @@ export default function Home() {
             An app for quick reminders to stay connected with your closest
             personal and professional contacts
           </p>
-          <Button className="mt-2 text-blue-400">Click Here</Button>
+          <Dialog>
+            <DialogTrigger>
+              <Button className="mt-2 text-blue-400">Click Here</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add your Catchup</DialogTitle>
+                <DialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
       </main>
     </div>
