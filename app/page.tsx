2a2a01df92f4658798a41a97d89bb430b9bd1a8a
Anil Antony <anilantony3134@gmail.com>
2023-12-05 00:41:02 +0530

    chore: added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import HeadSEO from "../components/HeadSEO";
+import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <HeadSEO />
       <main>
-        <div className="h-100">
-          <div className="flex h-100 justify-center items-center">CatchUp</div>
+        <div className="min-h-screen flex flex-col justify-center text-white items-center bg-black">
+          <h1 className="font-bold text-4xl">CatchUp</h1>
+          <p className="w-80 text-center text-sm my-4">
+            An app for quick reminders to stay connected with your closest
+            personal and professional contacts
+          </p>
+          <Button className="mt-2 text-blue-400">Click Here</Button>
         </div>
       </main>
     </div>
