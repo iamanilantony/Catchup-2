042570e9095aa6c246bb61efc5a1980ae2410e33
Anil Antony <anilantony3134@gmail.com>
2023-12-06 07:31:15 +0530

    chore: home cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,18 @@
-"use client";
-
-import HeadSEO from "../components/HeadSEO";
-import { Button } from "../components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger
-} from "../components/ui/dialog";
+import Link from "next/link";
+// import HeadSEO from "../components/HeadSEO";
+import { buttonVariants } from "../components/ui/Button";
 
 export default function Home() {
   return (
-    <div>
-      <HeadSEO />
-      <main>
-        <div className="min-h-screen flex flex-col justify-center text-white items-center bg-black">
-          <h1 className="font-bold text-4xl">CatchUp</h1>
-          <p className="w-80 text-center text-sm my-4">
-            An app for quick reminders to stay connected with your closest
-            personal and professional contacts
-          </p>
-          <Dialog>
-            <DialogTrigger>
-              <Button className="mt-2 text-blue-400">Click Here</Button>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Add your Catchup</DialogTitle>
-                <DialogDescription>
-                  This action cannot be undone. This will permanently delete
-                  your account and remove your data from our servers.
-                </DialogDescription>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
-        </div>
-      </main>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="font-bold text-4xl">CatchUp</h1>
+      <p className="w-80 text-center text-sm my-4">
+        An app for quick reminders to stay connected with your closest personal
+        and professional contacts
+      </p>
+      <Link href="/home" className={buttonVariants({ variant: "default" })}>
+        Start
+      </Link>
     </div>
   );
 }
