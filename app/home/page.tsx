61e13a92464ed7dc777251109faced868396a01d
iamanilantony <anilantony3134@gmail.com>
2023-12-08 06:38:31 +0530

    chore: cleanup

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,92 +1,6 @@
-import { Button, buttonVariants } from "../../components/ui/Button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogFooter,
-  DialogTrigger
-} from "../../components/ui/Dialog";
-
-import { Input } from "../../components/ui/Input";
-import { Label } from "../../components/ui/Label";
 import { ProfileCard } from "../../components/cards/ProfileCard";
-
-const users = [
-  {
-    name: "Anil Antony",
-    category: "Family",
-    lastCaughtUp: "2 weeks Ago",
-    notes: "remind about the trip",
-    image: "/char1/expr1.webp",
-    notification: true
-  },
-  {
-    name: "Shruthi",
-    category: "Family",
-    lastCaughtUp: "2 weeks Ago",
-    notes: "remind about the trip",
-    image: "/char2/expr1.webp",
-    notification: false
-  },
-  {
-    name: "Vinay Kumar",
-    category: "Work",
-    lastCaughtUp: "2 weeks Ago",
-    notes: "remind about the trip",
-    image: "/char3/expr1.webp",
-    notification: false
-  },
-  {
-    name: "Ajay Babu",
-    category: "Friends",
-    lastCaughtUp: "2 weeks Ago",
-    notes: "remind about the trip",
-    image: "/char1/expr1.webp",
-    notification: false
-  },
-  {
-    name: "Avinash Toppo",
-    category: "Friends",
-    lastCaughtUp: "2 weeks Ago",
-    notes: "remind about the trip",
-    image: "/char2/expr2.webp",
-    notification: true
-  },
-  {
-    name: "Shubham Shinde",
-    category: "Friends",
-    lastCaughtUp: "2 weeks Ago",
-    notes: "remind about the trip",
-    image: "/char3/expr3.webp",
-    notification: true
-  },
-  {
-    name: "Arjun Sineed",
-    category: "Friends",
-    lastCaughtUp: "2 weeks Ago",
-    notes: "remind about the trip",
-    image: "/char1/expr1.webp",
-    notification: true
-  },
-  {
-    name: "Aadil Shah",
-    category: "Friends",
-    lastCaughtUp: "2 weeks Ago",
-    notes: "remind about the trip",
-    image: "/char2/expr1.webp",
-    notification: true
-  },
-  {
-    name: "Vaishnav",
-    category: "Friends",
-    lastCaughtUp: "2 weeks Ago",
-    notes: "remind about the trip",
-    image: "/char3/expr1.webp",
-    notification: true
-  }
-];
+import { AddUserModal } from "../../components/AddUserModal";
+import users from "./data";
 
 export default function Page() {
   return (
@@ -96,42 +10,7 @@ export default function Page() {
           Hello, Dashboard Page!
         </h1>
         <div className="flex flex-col">
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button>Click Here</Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Add FollowUp</DialogTitle>
-                <DialogDescription>Create your FollowUp</DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="name" className="text-right">
-                    Name
-                  </Label>
-                  <Input
-                    id="name"
-                    value="Pedro Duarte"
-                    className="col-span-3"
-                  />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="username" className="text-right">
-                    Username
-                  </Label>
-                  <Input
-                    id="username"
-                    value="@peduarte"
-                    className="col-span-3"
-                  />
-                </div>
-              </div>
-              <DialogFooter>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
+          <AddUserModal />
         </div>
       </div>
       <div className="flex flex-wrap justify-between">
---
+++ components/AddUserModal.tsx
@@ -0,0 +1,45 @@
+import { Button, buttonVariants } from "./ui/Button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+  DialogTrigger
+} from "./ui/Dialog";
+import { Label } from "./ui/Label";
+import { Input } from "./ui/Input";
+
+export const AddUserModal = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Click Here</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add FollowUp</DialogTitle>
+          <DialogDescription>Create your FollowUp</DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
