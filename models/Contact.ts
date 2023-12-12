645bae47fc8853a7cff45084bc691e066aa51956
iamanilantony <anilantony3134@gmail.com>
2023-12-13 05:03:19 +0530

    chore: conatct model changes

--- models/Contact.ts
+++ models/Contact.ts
@@ -1,32 +1,46 @@
-import {Document, Schema, models, model} from 'mongoose';
+import mongoose, { Document, Schema, models, model } from "mongoose";
 
-export interface ContactDocument extends Document{
-    id: string,
-    name: string,
-    relationship: string,
-    duration: string,
-} 
+export interface ContactDocument extends Document {
+  id: string;
+  name: string;
+  relationship: string;
+  duration: string;
+}
 
 const ContactSchema = new Schema({
-    id: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    relationship: {
-        type: String,
-        required: true
-    },
-    duration: {
-        type: String,
-        enum: ['daily','every-other-day','every-three-day','weekly', 'bi-weekly', 'monthly', 'bi-monthly', 'quarterly', 'semi-annually'],
-        required: true,
-        default: 'monthly',
-    },
+  userName: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users"
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  relationship: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: String,
+    enum: [
+      "daily",
+      "every-other-day",
+      "every-three-day",
+      "weekly",
+      "bi-weekly",
+      "monthly",
+      "bi-monthly",
+      "quarterly",
+      "semi-annually"
+    ],
+    required: true,
+    default: "monthly"
+  }
 });
 
-export default models.Contact || model<ContactDocument>('Contact', ContactSchema);
+export default models.Contact ||
+  model<ContactDocument>("contacts", ContactSchema);
