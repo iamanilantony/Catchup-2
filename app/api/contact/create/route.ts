160796938fe28c0e2ad7b266f30b69ae6c697f95
iamanilantony <anilantony3134@gmail.com>
2023-12-12 06:37:21 +0530

    chore: post request added

--- app/api/contact/create/route.ts
+++ app/api/contact/create/route.ts
@@ -1,22 +1,22 @@
 import connectDB from "@/lib/dbNativeConnect";
 import { ContactValidator } from "@/lib/validators/contact";
 import { NextResponse } from "next/server";
+import { z } from "zod";
 
 export async function POST(req: Request, res: Response) {
   const db = await connectDB();
   const body = await req.json();
 
   const { name, relationship, duration } = ContactValidator.parse(body);
-  console.log(name, relationship, duration);
-
   try {
-    const contact = await db
-      .collection("contacts")
-      .insertOne({ name, relationship, duration });
-    console.log(contact);
+    await db.collection("contacts").insertOne({ name, relationship, duration });
     return new Response("added contact", { status: 200 });
-  } catch (e) {
-    console.error(e);
-    return new Response("Failed to add contact", { status: 404 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request data passed", { status: 422 });
+    }
+    return new Response("Could not contact, please try again later", {
+      status: 500
+    });
   }
 }
--- app/api/contact/list/route.ts
+++ app/api/contact/list/route.ts
@@ -1,16 +1,22 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import dbConnect from '@/lib/dbConnect';
-import Contact,  { ContactDocument }  from "@/models/Contact";
+import { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "@/lib/dbConnect";
+import Contact, { ContactDocument } from "@/models/Contact";
+import connectToMongoDB from "@/lib/dbNativeConnect";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    await dbConnect();
-
-    try {
-        const contacts: ContactDocument[]  = await (Contact as any).find({}) as ContactDocument[];
-        console.log(contacts,'worse')
-        res.status(200).json({ status: 'success', data: contacts });
-    } catch (e) {
-        console.error(e);
-        res.status(404).json({ status: 'error', message: 'Recipe search could not be performed.' });
-    }
+export async function GET(req: Request, res: NextApiResponse) {
+  const db = await connectToMongoDB();
+  const body = await req.json();
+  try {
+    const contacts: ContactDocument[] = (await (Contact as any).find(
+      {}
+    )) as ContactDocument[];
+    console.log(contacts, "worse");
+    res.status(200).json({ status: "success", data: contacts });
+  } catch (e) {
+    console.error(e);
+    res.status(404).json({
+      status: "error",
+      message: "Recipe search could not be performed."
+    });
+  }
 }
