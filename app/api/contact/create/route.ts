4aff2c7f04213c7d568109ac1acc4f039f752fc8
iamanilantony <anilantony3134@gmail.com>
2023-12-13 05:04:10 +0530

    fix: create roue fix

--- app/api/contact/create/route.ts
+++ app/api/contact/create/route.ts
@@ -1,17 +1,27 @@
-import connectDB from "@/lib/db/dbNativeConnect";
 import { ContactValidator } from "@/lib/validators/contact";
-import { NextResponse } from "next/server";
+import Contact from "@/models/Contact";
+import mongoose from "mongoose";
 import { z } from "zod";
+import dbConnect from "@/lib/db/dbConnect";
 
 export async function POST(req: Request, res: Response) {
-  const db = await connectDB();
   const body = await req.json();
 
   const { name, relationship, duration } = ContactValidator.parse(body);
   try {
-    await db.collection("contacts").insertOne({ name, relationship, duration });
+    await dbConnect();
+    const userIdString = "6577bca33f6054ef3ef47027";
+    const userId = new mongoose.Types.ObjectId(userIdString);
+    const contact = new Contact({
+      userName: userId,
+      name,
+      relationship,
+      duration
+    });
+    await contact.save();
     return new Response("added contact", { status: 200 });
   } catch (error) {
+    console.log(error);
     if (error instanceof z.ZodError) {
       return new Response("Invalid request data passed", { status: 422 });
     }
