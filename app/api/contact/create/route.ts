05c78c9bbfb497f02dd1dadacaf3b4c0069368c7
iamanilantony <anilantony3134@gmail.com>
2024-03-17 12:26:01 +0530

    chore: fix

--- app/api/contact/create/route.ts
+++ app/api/contact/create/route.ts
@@ -13,42 +13,42 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
   // const body = await req.json();
-  const { name, relationship, duration } = {};
-  try {
-    const db = await connectDB();
-    // const { getUser } = getKindeServerSession();
-    // const session2 = await geSession(req, res, authOptions);
-    // console.log(session2)
-    const session = await getServerSession(authOptions);
-    console.log(session, "minimini");
-    const _id = new ObjectId();
-    const index = Math.floor(Math.random() * 11);
-    console.log(session, _id);
-    const avatar = avatars[index];
-    try {
-      await db.collection("contacts").insertOne({
-        _id,
-        name,
-        relationship,
-        duration,
-        // userName: session.user?.id,
-        avatar,
-        lastContacted: new Date(),
-        startFrom: new Date(),
-        dayToContact: "monday",
-        removed: false,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-    return new Response("added contact", { status: 200 });
-  } catch (error) {
-    console.log(error);
-    if (error instanceof z.ZodError) {
-      return new Response("Invalid request data passed", { status: 422 });
-    }
-    return new Response("Could not contact, please try again later", {
-      status: 500,
-    });
-  }
+  // const { name, relationship, duration } = {};
+  // try {
+  //   const db = await connectDB();
+  //   // const { getUser } = getKindeServerSession();
+  //   // const session2 = await geSession(req, res, authOptions);
+  //   // console.log(session2)
+  //   const session = await getServerSession(authOptions);
+  //   console.log(session, "minimini");
+  //   const _id = new ObjectId();
+  //   const index = Math.floor(Math.random() * 11);
+  //   console.log(session, _id);
+  //   const avatar = avatars[index];
+  //   try {
+  //     await db.collection("contacts").insertOne({
+  //       _id,
+  //       name,
+  //       relationship,
+  //       duration,
+  //       // userName: session.user?.id,
+  //       avatar,
+  //       lastContacted: new Date(),
+  //       startFrom: new Date(),
+  //       dayToContact: "monday",
+  //       removed: false,
+  //     });
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  //   return new Response("added contact", { status: 200 });
+  // } catch (error) {
+  //   console.log(error);
+  //   if (error instanceof z.ZodError) {
+  //     return new Response("Invalid request data passed", { status: 422 });
+  //   }
+  //   return new Response("Could not contact, please try again later", {
+  //     status: 500,
+  //   });
+  // }
 }
