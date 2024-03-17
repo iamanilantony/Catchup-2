86616a0b16d37755b11ca3c601ab37c6e8c1b37a
iamanilantony <anilantony3134@gmail.com>
2024-03-17 10:27:14 +0530

    chore: added form and ui elements shadcn

---
+++ data/callFrequency.ts
@@ -0,0 +1,9 @@
+export default [
+    {code: 'weekly', label: 'Weekly'},
+    {code: 'bi-weekly', label: 'Bi-Weekly'},
+    {code: 'monthly', label: 'Monthly'},
+    {code: 'bi-monthly', label: 'Bi-Monthly'},
+    {code: 'quarterly', label: 'Quarterly'},
+    {code: 'half-yearly', label: 'Half Yearly'},
+    {code: 'yearly', label: 'Yearly'},
+]
---
+++ data/categories.ts
@@ -0,0 +1,5 @@
+export default [
+    {code: 'family', label: 'Family'},
+    {code: 'professional', label: 'Professional'},
+    {code: 'friends', label: 'Friends'},
+]
